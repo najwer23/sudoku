b8f45057063d5c9546af6e9ddba85f2c3bb1b6cf
Mariusz Najwer <najwer23@gmail.com>
2023-12-21 22:52:00 +0100

    Board with events

--- src/sudoku/SudokuBoard.styled.ts
+++ src/sudoku/SudokuBoard.styled.ts
@@ -1,37 +1,75 @@
 import styled, { css } from "styled-components";
 
+const item = `calc(11.111111111% - 3.1111111px)`;
+
 export const BoardBox = styled.div`
-	width: 280px;
+	width: 90%;
+	max-width: 500px;
 	display: flex;
 	flex-wrap: wrap;
 	margin: 10px;
 `;
 
 export const BoardBoxItem = styled.div`
-	width: 31px;
-	height: 30px;
-	text-align: center;
-	line-height: 30px;
+	width: calc(${item} + 2px);
+	aspect-ratio: 1;
+	box-sizing: content-box;
 	border-left: 1px solid grey;
 	border-top: 1px solid grey;
-	cursor: pointer;
+
 	-webkit-user-select: none; /* Safari */
 	-ms-user-select: none; /* IE 10 and IE 11 */
 	user-select: none; /* Standard syntax */
 
 	&:nth-child(9n) {
-		width: 32px;
+		width: calc(${item} + 2px);
 		border-right: 1px solid grey;
 	}
 
 	&:nth-last-child(-n + 9) {
-		width: 31px;
+		width: calc(${item} + 2px);
 		border-bottom: 1px solid grey;
 	}
 
 	&:nth-last-child(-n + 1) {
-		width: 32px;
+		width: calc(${item} + 2px);
 	}
 `;
 
-export const BoardBoxItemValue = styled.div``
+export const BoardBoxItemValueGen = styled.div`
+	background-color: lightgray;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: clamp(1rem, 0.562vw + 0.98rem, 1.35rem);
+`;
+
+export const BoardBoxItemValue = styled(BoardBoxItemValueGen)`
+	cursor: pointer;
+	background-color: white;
+`;
+
+export const BoardNumbers = styled.div`
+	display: flex;
+	gap: 5px;
+	margin: 10px;
+	flex-wrap: wrap;
+`;
+
+export const TextWrapper = styled.div`
+	margin: 10px;
+`;
+
+export const BoardNumber = styled.div<{ $active: boolean }>`
+	height: 50px;
+	width: 50px;
+	line-height: 50px;
+	text-align: center;
+	background-color: ${(props) => (props.$active ? "orangered" : "orange")};
+	-webkit-user-select: none; /* Safari */
+	-ms-user-select: none; /* IE 10 and IE 11 */
+	user-select: none; /* Standard syntax */
+	cursor: pointer;
+`;
--- src/sudoku/SudokuBoard.tsx
+++ src/sudoku/SudokuBoard.tsx
@@ -1,18 +1,47 @@
-import { BoardBox, BoardBoxItem, BoardBoxItemValue } from "./SudokuBoard.styled"
+import { useState } from "react"
+import { BoardBox, BoardBoxItem, BoardBoxItemValue, BoardBoxItemValueGen, BoardNumber, BoardNumbers, TextWrapper } from "./SudokuBoard.styled"
 
 export const SudokuBoard = ({ board, setBoard }: SudokuBoardType) => {
+	const [number, setNumber] = useState(1);
 
-	console.log(board)
+	const onClickBoardItem = (index: number, value: string) => () => {
+		const newValue = board.map((v, i) => (i === index) ? "a" + value : v)
+		setBoard(newValue)
+	}
+
+	const onClikcBoardNumber = (n: number) => () => setNumber(n)
+
 	return (
-		<BoardBox>
-			{board.map((value, index) => (
-				<BoardBoxItem key={"box-item" + index}>
-					<BoardBoxItemValue key={"box-value" + index}>
-						{value === "" ? "" : value}
-					</BoardBoxItemValue>
-				</BoardBoxItem>
+		<>
+			<TextWrapper>
+				<h1>Sudoku</h1>
+			</TextWrapper>
 
-			))}
-		</BoardBox>
+			<BoardBox>
+				{board.map((value, index) => (
+					<BoardBoxItem key={"box-item" + index}>
+						{value === "" || value.includes("a") ?
+							<BoardBoxItemValue key={"box-value" + index} onClick={onClickBoardItem(index, String(number))}
+							>{value.slice(1)}
+							</BoardBoxItemValue> :
+							<BoardBoxItemValueGen key={"box-value" + index}>{value}</BoardBoxItemValueGen>
+						}
+
+					</BoardBoxItem>
+				))}
+			</BoardBox>
+
+			<TextWrapper>
+				<p>Chosen Number</p>
+			</TextWrapper>
+
+			<BoardNumbers>
+				{Array(9)
+					.fill(null)
+					.map((_, i) => <BoardNumber onClick={onClikcBoardNumber(i + 1)} $active={(i + 1) === number}>{i + 1}
+					</BoardNumber>)}
+			</BoardNumbers>
+
+		</>
 	)
 }
